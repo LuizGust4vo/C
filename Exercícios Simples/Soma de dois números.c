645ef542ad3f116734f6a6685f498f9a653bf05f
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-07-05 14:51:11 -0300

    Add files via upload

---
+++ Exercícios Simples/Soma de dois números.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+int n1,n2;
+int soma;
+
+printf("Digite um Número: ");
+scanf("%i",&n1);
+
+printf("Digite um Número: ");
+scanf("%i",&n2);
+
+soma=n1+n2;
+printf("A soma é igual= %i",soma);
+
+return 0;
+}
