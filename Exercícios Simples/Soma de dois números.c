0750f9dadd99481e3b1d17fbd212dd2cf7c0566a
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-07-05 21:47:50 -0300

    Update Soma de dois números.c

--- Exercícios Simples/Soma de dois números.c
+++ Exercícios Simples/Soma de dois números.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <locale.h>
 
-int main(){
-setlocale(LC_ALL, "");
+int main()
+{
+    setlocale(LC_ALL, "");
 
-int n1, n2;
-int soma;
+    int n1,n2;
+    int soma;
 
-printf("Digite um Número: ");
-scanf("%i", &n1);
+    printf("Digite um Número: ");
+    scanf("%i",&n1);
 
-printf("Digite um Número: ");
-scanf("%i", &n2);
+    printf("Digite um Número: ");
+    scanf("%i",&n2);
 
-soma = n1 + n2;
-printf("A soma é igual = %i", soma);
+    soma=n1+n2;
+    printf("\nO resultado da soma é = %i\n",soma);
 
-return 0;
+    return 0;
 }
