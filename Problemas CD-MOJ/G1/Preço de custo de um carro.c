accea99a6b311c0b790b5da93987ac4d28857aba
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-07-07 17:45:03 -0300

    Update Preço de custo de um carro.c

--- Problemas CD-MOJ/G1/Preço de custo de um carro.c
+++ Problemas CD-MOJ/G1/Preço de custo de um carro.c
@@ -17,9 +17,17 @@ int main ()
     printf ("Porcentagem de impostos a serem pagos: ");
     scanf ("%d", &I);
 
-    cost = (price / (100 + (L + I))) * 100;
+    if (price >= 0 && L >= 0 && I >= 0 && price <= pow(10, 9) && L <= 100 && I <= 100)
+    {
+        cost = (price / (100 + (L + I))) * 100;
 
-    printf ("\nPreço de custo de fabricação = %lf", cost);
+        printf ("\nPreço de custo de fabricação = %lf\n", cost);
+    }
+
+    else
+    {
+        printf ("\nValor(es) digitado(s) inválido(s)!\n");
+    }
 
     return 0;
 }
