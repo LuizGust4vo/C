df31a80af46993bcba42848215113e20c10ec8c2
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-07-07 13:36:52 -0300

    Update precofinal_consumidor.c

--- Problemas CD-MOJ/F1/precofinal_consumidor.c
+++ Problemas CD-MOJ/F1/precofinal_consumidor.c
@@ -7,7 +7,20 @@ int main ()
     double cost, price;
     int L, I;
 
-  
+    printf ("Digite o custo de fabricação do veículo: ");
+    scanf ("%lf", &cost);
+    printf ("Digite a porcentagem de lucro para a distribuidora: ");
+    scanf ("%d", &L);
+    printf ("Digite a porcentagem de impostos a serem pagos: ");
+    scanf ("%d", &I);
+
+    price = cost + (cost / 100 * L) + (cost / 100 *I);
+
+    printf ("\ncost = %lf", cost);
+    printf ("\nL = %d", L);
+    printf ("\nI = %d", I);
+    printf ("\nPreço final a ser pago pelo consumidor: %.2lf", price);
+
 
     return 0;
 }
