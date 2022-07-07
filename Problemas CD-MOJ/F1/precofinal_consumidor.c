d92825bf80280fe23dbf6c1690fe7445c200fddd
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-07-07 13:49:19 -0300

    Update precofinal_consumidor.c

--- Problemas CD-MOJ/F1/precofinal_consumidor.c
+++ Problemas CD-MOJ/F1/precofinal_consumidor.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <locale.h>
+#include <math.h>
 
 int main ()
 {
@@ -7,20 +8,24 @@ int main ()
     double cost, price;
     int L, I;
 
-    printf ("Digite o custo de fabricação do veículo: ");
+    printf ("Digite o custo de fabricação do veículo (R$): ");
     scanf ("%lf", &cost);
     printf ("Digite a porcentagem de lucro para a distribuidora: ");
     scanf ("%d", &L);
     printf ("Digite a porcentagem de impostos a serem pagos: ");
     scanf ("%d", &I);
 
-    price = cost + (cost / 100 * L) + (cost / 100 *I);
+    if (cost >= 0 && L >= 0 && I >= 0 && cost <= pow(10, 9) && L <= 100 & I <= 100)
+    {
+        price = cost + (cost / 100 * L) + (cost / 100 *I);
 
-    printf ("\ncost = %lf", cost);
-    printf ("\nL = %d", L);
-    printf ("\nI = %d", I);
-    printf ("\nPreço final a ser pago pelo consumidor: %.2lf", price);
+        printf ("\nPreço final a ser pago pelo consumidor = R$ %.2lf\n", price);
+    }
 
+    else
+    {
+        printf ("\nValor(es) informado(s) inválido(s)!\n");
+    }
 
     return 0;
 }
