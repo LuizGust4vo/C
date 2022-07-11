b8d84a9423e97a590b7a437fcce9989dc72b8fe0
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-07-11 16:10:48 -0300

    Update Zerinho ou um.c

--- Problemas CD-MOJ/Lab2-T4/B1/Zerinho ou um.c
+++ Problemas CD-MOJ/Lab2-T4/B1/Zerinho ou um.c
@@ -1,21 +1,49 @@
 #include <stdio.h>
 #include <locale.h>
 
-int main(){
+int main()
+{
 
-setlocale (LC_ALL, "");
+    setlocale (LC_ALL, "");
 
-int A, B, C;
+    int A, B, C;
 
-printf ("Zerinho ou... Um!");
-scanf ("%d%d%d", &A, &B, &C);
+    printf ("----- Zerinho ou... Um! -----\n\n");
+    scanf ("%d%d%d", &A, &B, &C);
 
-if (A == 0 || B == 0 || C == 0 || A == 1 || B == 1 || C == 1){
-    if (){
+    if (A == 0 || A == 1 )
+    {
+        if (B == 0 || B == 1)
+        {
+            if (C == 0 || C == 1)
+            {
+                if (A != B && A != C)
+                {
+                    printf ("\nA\n");
+                }
+                else if (B != A && B != C)
+                {
+                    printf ("\nB\n");
+                }
+                else if (C != A && C != B)
+                {
+                    printf ("\nC\n");
+                }
+                else if (A == B && B == C)
+                {
+                    printf ("\nEmpate!\n");
 
-
+                }
+            }
+        }
     }
-}
 
-return 0;
+    if (A != 0 || A != 1 || B != 0 || B != 1 || C != 0 || C != 1)
+    {
+        printf ("\nValor(es) inválido(s)!\n");
+    }
+
+
+
+    return 0;
 }
