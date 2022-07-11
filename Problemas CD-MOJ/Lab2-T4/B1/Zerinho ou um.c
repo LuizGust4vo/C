7be4cb34343090309715058bb928be766339856c
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-07-11 11:25:16 -0300

    Create Zerinho ou um.c

---
+++ Problemas CD-MOJ/Lab2-T4/B1/Zerinho ou um.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+
+setlocale (LC_ALL, "");
+
+int A, B, C;
+
+printf ("Zerinho ou... Um!");
+scanf ("%d%d%d", &A, &B, &C);
+
+if (A == 0 || B == 0 || C == 0 || A == 1 || B == 1 || C == 1){
+    if (){
+
+
+    }
+}
+
+return 0;
+}
