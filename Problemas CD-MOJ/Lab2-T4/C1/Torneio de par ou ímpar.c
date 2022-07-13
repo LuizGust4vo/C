4dc85f845aa5de82f8e7e09057e0ad0996b2f4a9
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-07-12 21:13:27 -0300

    Update Torneio de par ou ímpar.c

--- Problemas CD-MOJ/Lab2-T4/C1/Torneio de par ou ímpar.c
+++ Problemas CD-MOJ/Lab2-T4/C1/Torneio de par ou ímpar.c
@@ -1,83 +1,33 @@
 #include <stdio.h>
-#include <locale.h>
 
 int main ()
 {
-    setlocale (LC_ALL, "");
 
-    int E1, A, B, S1;
-    int E2, C, D, S2;
-    int E3, G1, G2, S3;
+    int E, P1, P2;
+    char G1, G2;
 
-    scanf ("%d %d %d", &E1, &A, &B);
-    scanf ("%d %d %d", &E2, &C, &D);
-
-    S1 = A + B;
-
-    if (E1 == 0 && S1%2 == 0){
-        printf ("A ganhou, par!");
-        G1 = A;
-    }
-    else if (E1 == 0 && S1%2 != 0){
-        printf ("B ganhou, ímpar!");
-        G1 = B;
-    }
-    if (E1 == 1 && S1%2 == 0){
-        printf ("B ganhou, par!");
-        G1 = B;
-    }
-    else if (E1 == 1 && S1%2 != 0){
-        printf ("A ganhou, ímpar!");
-        G1 = A;
-    }
-
-    S2 = C + D;
-
-    if (E2 == 0 && S2%2 == 0){
-        printf ("\nC ganhou, par!");
-        G2 = C;
-    }
-    else if (E2 == 0 && S2%2 != 0){
-        printf ("\nD ganhou, ímpar!");
-        G2 = D;
-    }
-    if (E2 == 1 && S2%2 == 0){
-        printf ("\nD ganhou, par!");
-        G2 = D;
-    }
-    else if (E2 == 1 && S2%2 != 0){
-        printf ("\nC ganhou, ímpar!");
-        G2 = C;
-    }
-
-
-    if (G1 == A && G2 == C){
-        scanf ("%d %d %d", &E3, &G1, &G2);
-        S3 = G1 + G2;
-        if (E3 == 0 && S3%2 == 0){
-            printf ("A");
+    scanf ("%d %d %d", &E, &P1, &P2);
+    if (E == 0 || E == 1){
+    if (P1 >= 1 && P1 <= 10 && P2 >= 1 && P2 <= 10){
+    if ((P1 + P2) % 2 == E) G1 = 'A';
+    else G1 = 'B';
     }}
 
-else if (G1 == A && G2 == D){
-    scanf ("%d %d %d", &E3, &G1, &G2);
-    S3 = G1 + G2;
-    if (E3 == 0 && S3%2 != 0){
-        printf ("D");
-}}
+    scanf ("%d %d %d", &E, &P1, &P2);
+    if (E == 0 || E == 1){
+    if (P1 >= 1 && P1 <= 10 && P2 >= 1 && P2 <= 10){
+    if ((P1 + P2) % 2 == E) G2 = 'C';
+    else G2 = 'D';
+    }}
 
-else if (G2 == B && G2 == C){
-    scanf ("%d %d %d", &E3, &G1, &G2);
-    S3 = G1 + G2;
-    if (E3 == 0 && S3%2 == 0){
-        printf ("B");
-}}
-
-else if (G2 == B && G2 == D){
-    scanf ("%d %d %d", &E3, &G1, &G2);
-    S3 = G1 + G2;
-    if (E3 == 0 && S3%2 != 0){
-        printf ("D");
-}}
+    scanf ("%d %d %d", &E, &P1, &P2);
+    if (E == 0 || E == 1){
+    if (P1 >= 1 && P1 <= 10 && P2 >= 1 && P2 <= 10){
+    if ((P1 + P2) % 2 == E)
+    printf ("%c\n", G1);
+    else
+    printf ("%c\n", G2);
+    }}
 
 return 0;
 }
