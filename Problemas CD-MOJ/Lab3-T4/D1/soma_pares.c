b74b48fde2b8774c3d174d3f4cb4d3e21cfab2ea
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-07-20 23:30:55 -0300

    Update soma_pares.c

--- Problemas CD-MOJ/Lab3-T4/D1/soma_pares.c
+++ Problemas CD-MOJ/Lab3-T4/D1/soma_pares.c
@@ -6,6 +6,20 @@ int main ()
 
     setlocale (LC_ALL, "");
 
+    int n = 0, soma = 0;
+
+    if (n == 0)
+    {
+        do
+        {
+            printf ("Digite um número: ");
+            scanf ("%d", &n);
+            if (n % 2 == 0) soma += n;
+        }
+        while (n != 0);
+    }
+
+    printf ("\nSoma dos números pares digitados: %d\n", soma);
 
     return 0;
 }
