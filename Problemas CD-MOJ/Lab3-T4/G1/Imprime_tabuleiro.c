2bc54f6e1d0a73d9f57b8ce586fa78525744eb50
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-07-21 01:28:03 -0300

    Update Imprime_tabuleiro.c

--- Problemas CD-MOJ/Lab3-T4/G1/Imprime_tabuleiro.c
+++ Problemas CD-MOJ/Lab3-T4/G1/Imprime_tabuleiro.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <locale.h>
 
-int main (){
+int main ()
+{
 
-setlocale (LC_ALL, "");
+    setlocale (LC_ALL, "");
 
-int L, l, C, c;
+    int L, l, C, c;
 
-printf("Digite o número de linhas da matriz: ");
-scanf ("%d", &L);
-printf ("Digite o número de colunas da matriz: ");
-scanf ("%d", &C);
+    printf("Digite o número de linhas da matriz: ");
+    scanf ("%d", &L);
+    printf ("Digite o número de colunas da matriz: ");
+    scanf ("%d", &C);
 
-printf ("\n");
-
-for (l = 0; l < L; l++){
-    for (c = 0; c < C; c++){
-        printf ("[%03d, %03d] ", l, c);
-    }
     printf ("\n");
-}
 
-return 0;
+    for (l = 0; l < L; l++)
+    {
+        for (c = 0; c < C; c++)
+        {
+            printf ("[%03d, %03d] ", l, c);
+        }
+        printf ("\n");
+    }
+
+    return 0;
 }
