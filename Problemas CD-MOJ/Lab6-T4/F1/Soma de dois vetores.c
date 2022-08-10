61f2ebcab53848a00825d304b7e3259c1c2f5ab6
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-08-10 01:04:43 -0300

    Update Soma de dois vetores.c

--- Problemas CD-MOJ/Lab6-T4/F1/Soma de dois vetores.c
+++ Problemas CD-MOJ/Lab6-T4/F1/Soma de dois vetores.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main (){
+
+int T;
+
+    scanf ("%d", &T);
+    int v1[T], v2[T], vsoma[T];
+
+    for (int cont = 0; cont < T; cont++)
+    {
+        scanf ("%d", &v1[cont]);
+    }
+
+    for (int cont = 0; cont < T; cont++)
+    {
+        scanf ("%d", &v2[cont]);
+    }
+
+    for (int cont = 0; cont < T; cont++)
+    {
+    vsoma[cont] = v1[cont] + v2[cont];
+    printf ("%d ", vsoma[cont]);
+    }
+
+
+return 0;
+}
