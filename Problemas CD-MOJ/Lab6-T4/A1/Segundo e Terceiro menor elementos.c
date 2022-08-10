d61473db62c4fd138a8ca6e0c3f4ca7c83dbd5ca
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-08-09 23:45:00 -0300

    Update Segundo e Terceiro menor elementos.c

--- Problemas CD-MOJ/Lab6-T4/A1/Segundo e Terceiro menor elementos.c
+++ Problemas CD-MOJ/Lab6-T4/A1/Segundo e Terceiro menor elementos.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main ()
+{
+    int T, m, sm, tm;
+
+    scanf ("%d", &T);
+    int v1[T];
+
+    for (int cont = 0; cont < T; cont++)
+    {
+        scanf ("%d", &v1[cont]);
+
+        if (cont == 0) m = v1[cont];
+        if (v1[cont] < m){
+        tm = sm;
+        sm = m;
+        m = v1[cont];
+        }
+
+    if (v1[cont] > m && v1[cont] < sm){
+    tm = sm;
+    sm = v1[cont];
+    }
+    if (v1[cont] > sm && v1[cont] < tm) tm = v1[cont];
+    }
+
+    printf ("%d\n%d", sm, tm);
+
+    return 0;
+}
