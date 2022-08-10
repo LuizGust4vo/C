62875be46e1beca0f7010e455f1e9520db981129
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-08-10 00:51:58 -0300

    Update Contido no vetor.c

--- Problemas CD-MOJ/Lab6-T4/E1/Contido no vetor.c
+++ Problemas CD-MOJ/Lab6-T4/E1/Contido no vetor.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main ()
+{
+    int T, x, cont2 = 0;
+
+    scanf ("%d", &T);
+    int v1[T];
+
+    for (int cont = 0; cont < T; cont++)
+    {
+        scanf ("%d", &v1[cont]);
+    }
+
+    scanf ("%d", &x);
+
+    for (int cont1 = 0; cont1 < T; cont1++)
+    {
+        if (v1[cont1] == x) printf ("pertence");
+        if (v1[cont1] != x) cont2++;
+        if (cont2 == T) printf ("nao pertence");
+    }
+
+    return 0;
+}
