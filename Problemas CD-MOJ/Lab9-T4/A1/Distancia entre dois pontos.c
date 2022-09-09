9df7b6ae667c1d66e83a95a46568cd38ac3da8f5
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-09-09 10:51:49 -0300

    Create Distancia entre dois pontos.c

---
+++ Problemas CD-MOJ/Lab9-T4/A1/Distancia entre dois pontos.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+struct tipoPonto {
+int xa = 4, ya = 3;
+int xb = 3, yb = -2;
+};
+
+double distancia (struct tipoPonto pa, struct tipoPonto pb){
+
+D = xb - xa;
+
+return D;
+}
+
+int main (){
+
+printf ("%lf", distancia(struct tipoPonto xb, struct tipoPonto xa));
+
+return 0;
+}
