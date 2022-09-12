992ea8b29971ec100278a472a2e7c4968f7fb8ce
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-09-12 15:38:00 -0300

    Create Separando informações.c

---
+++ Problemas CD-MOJ/Lab9-T4/C1/Separando informações.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct tipoData {
+int dia;
+int mes;
+int ano;
+};
+
+struct tipoPessoa separaInfo (char nome[80], int dt_nascimento){
+
+};
+
+char separaInfo 
+
+int main (){
+
+
+
+return 0;
+}
