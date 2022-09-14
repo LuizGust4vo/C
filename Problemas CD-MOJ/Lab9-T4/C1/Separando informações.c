fc828c44fd82537d7caad306770f34ff1d11e888
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-09-13 23:11:56 -0300

    Update Separando informações.c

--- Problemas CD-MOJ/Lab9-T4/C1/Separando informações.c
+++ Problemas CD-MOJ/Lab9-T4/C1/Separando informações.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 
-struct tipoData {
-int dia;
-int mes;
-int ano;
+struct tipoData
+{
+    int dia;
+    int mes;
+    int ano;
+};
+struct tipoPessoa
+{
+    char nome[80];
+    struct tipoData dtNascimento;
 };
 
-struct tipoPessoa separaInfo (char nome[80], int dt_nascimento){
+struct tipoPessoa separaInfo(char nome[80], int dt_nascimento)
+{
+    struct tipoPessoa x;
+    int i;
 
-};
+    for(i = 0; nome[i] != '\0'; i++)
+    {
+        x.nome[i] = nome[i];
+    }
+    x.nome[i] = '\0';
+    x.dtNascimento.ano = dt_nascimento/10000;
+    x.dtNascimento.mes = (dt_nascimento/100)%100;
+    x.dtNascimento.dia = dt_nascimento%100;
 
-char separaInfo 
-
-int main (){
-
-
-
-return 0;
+    return x;
 }
