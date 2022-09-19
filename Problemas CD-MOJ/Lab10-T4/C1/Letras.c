b48ea1c54cad6fc730b9c62fc729d71efc998ed6
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-09-18 22:55:51 -0300

    Update Letras.c

--- Problemas CD-MOJ/Lab10-T4/C1/Letras.c
+++ Problemas CD-MOJ/Lab10-T4/C1/Letras.c
@@ -3,6 +3,7 @@
 int conta_c (char str[], char c)
 {
     int qtd_c = 0, i;
+    
     for (i = 0; i < contaString(str); i++)
     {
         if (str[i] == c)
@@ -17,27 +18,32 @@ int conta_c (char str[], char c)
 int contaString (char str[])
 {
     int tamanho = 0;
+
     while (str[tamanho] != '\0')
     {
         tamanho++;
     }
+
     return tamanho;
 }
 
-int main (){
+int main ()
+{
+    char caracter;
+    char texto[1000];
+    double pp, cont = 0, cont1 = 0;
 
-char caracter;
-char texto[1000];
-double pp;
-int cont = 0;
+    scanf ("%c", &caracter);
 
-scanf ("%c", &caracter);
+    while (scanf ("%s", texto) != EOF)
+    {
+        cont += conta_c (texto, caracter);
+        cont1++;
+    }
 
-while (scanf ("%s", texto) != EOF){
-cont += conta_c (texto, caracter);
-}
+    pp = (cont * 100) / cont1;
 
-printf ("%d\n", cont);
+    printf ("%.1lf", pp);
 
-return 0;
+    return 0;
 }
