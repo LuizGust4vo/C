b375fd1eba0408a70bac1fa076fc7097fab6a6e1
Luiz Gustavo <lalmeida727.lg@gmail.com>
2023-09-13 02:53:25 -0300

    Create Transporte.c

---
+++ Revisão de APC - EDA/D/Transporte.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main ()
+{
+
+    int A, B, C;
+    int X, Y, Z;
+    int capacidade = 0;
+
+    scanf ("%d %d %d", &A, &B, &C);
+    
+    scanf ("%d %d %d", &X, &Y, &Z);
+
+
+    if (A > X || B > Y || C > Z)
+    {
+        printf ("%d\n", capacidade);
+    }
+
+    else
+    {
+        capacidade = (X / A) * (Y / B) * (Z / C);
+        printf ("%d\n", capacidade);
+    }
+
+    return 0;
+}
