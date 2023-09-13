4f6cb5eefebc00e6a70894b09fb36d522b583f5d
Luiz Gustavo <lalmeida727.lg@gmail.com>
2023-09-13 02:55:32 -0300

    Create Zerinho ou Um.c

---
+++ Revisão de APC - EDA/E/Zerinho ou Um.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main ()
+{
+
+    int a, b, c;
+
+    scanf ("%d %d %d", &a, &b, &c);
+
+    if (a == b && b == c){
+        printf ("empate\n");
+    }
+
+    else if (a != b && a != c){
+        printf ("A\n");
+    }
+
+
+    else if (b != a && b != c){
+        printf ("B\n");
+    }
+
+
+    else if (c != a && c != b){
+        printf ("C\n");
+    }
+
+    return 0;
+}
