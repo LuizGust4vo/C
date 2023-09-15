f45597af4efbd8f54c5d7a381f7e6aa96e01d575
Luiz Gustavo <lalmeida727.lg@gmail.com>
2023-09-14 23:58:59 -0300

    Create Vetor 3.c

---
+++ Revisão de APC - EDA/O/Vetor 3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main ()
+{
+
+    int n, v[10000];
+
+    scanf ("%d", &n);
+
+    for (int cont = 0; cont < n; cont++)
+    {
+        scanf ("%d", &v[cont]);
+    }
+
+    for (int cont1 = 0; cont1 < n; cont1++)
+    {
+        if (v[cont1] % 2 == 0) printf ("%d ", v[cont1]);
+        }
+
+    printf ("\n");
+
+    for (int cont2 = 0; cont2 < n; cont2++)
+    {
+        if (v[cont2] % 2 != 0) printf ("%d ", v[cont2]);
+        }
+
+    printf ("\n");
+
+    return 0;
+}
