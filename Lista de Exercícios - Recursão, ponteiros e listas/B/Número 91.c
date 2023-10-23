d9f408c325d2b3d5d66b2fcddf2dfa9db7168e22
Luiz Gustavo <lalmeida727.lg@gmail.com>
2023-10-22 23:04:03 -0300

    Update Número 91.c

--- Lista de Exercícios - Recursão, ponteiros e listas/B/Número 91.c
+++ Lista de Exercícios - Recursão, ponteiros e listas/B/Número 91.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+
+int f91(int N)
+{
+    if (N >= 101)
+    {
+        return N - 10;
+    }
+    else if (N > 0 && N <= 100)
+    {
+        return f91(f91(N + 11));
+    }
+}
+
+int main ()
+{
+
+    int n[250000], cont = 0;
+
+    do
+    {
+        scanf ("%d", &n[cont]);
+        cont++;
+    }
+    while (n[cont - 1] != 0);
+
+    for (int cont1 = 0; cont1 < cont - 1; cont1++)
+    {
+        printf ("f91(%d) = %d\n", n[cont1], f91(n[cont1]));
+    }
+
+    return 0;
+}
 
