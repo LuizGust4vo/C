82c21cf5bbe0a1ebed4d90be433fd681c0d051c0
LuizGust4vo <lalmeida727.lg@gmail.com>
2026-04-20 16:59:54 -0300

    vetor de structs.c

--- Revisão de C - EDA1/Revisão - Nilton e Fabricio/Vetor de Structs.c
+++ Revisão de C - EDA1/Revisão - Nilton e Fabricio/Vetor de Structs.c
@@ -5,11 +5,42 @@
 // Crie um vetor de 3 structs Aluno
 // Use ponteiros para preencher os dados
 
+typedef struct
+{
+    char nome[50];
+    int matricula;
+    float nota;
+} aluno;
+
 int main ()
 {
     setlocale (LC_ALL, "Portuguese");
 
+/* ----- INSERÇÃO DOS DADOS NA INICIALIZAÇÃO DA STRUCT -----
+    aluno aluno1[3] = {
+    "Luiz Gustavo", 221, 7.5,
+    "Eduardo Ferreira", 222, 6.0,
+    "Eric Silveira", 223, 8.5};
+*/
+
+    aluno aluno1[3];
+    for (int cont = 0; cont <= 2; cont++){
+    aluno *p = &aluno1[cont];
+
+// ----- INSERÇÃO DE DADOS PELO USUÁRIO -----
+    printf ("Digite o nome do aluno: ");
+    scanf ("%s", p -> nome);
+    printf ("Digite a matrícula: ");
+    scanf ("%d", &p -> matricula);
+    printf ("Digite a nota: ");
+    scanf ("%f", &p -> nota);
+    printf ("\n");
+    }
+
+    for (int cont = 0; cont <= 2; cont++){
+    aluno *p = &aluno1[cont];
+    printf ("Nome: %s\nMatrícula: %d\nNota: %.1f\n\n", p -> nome, p -> matricula, p -> nota); // Impressão pelo ponteiro
+    }
 
     return 0;
 }
-
